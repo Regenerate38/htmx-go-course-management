aab3199d4131241f1a34adb0aaf85ad1d870ece4
Regenerate38 <mukunda.adhikari@outlook.com>
2025-01-17 19:46:56 +0545

    Minor Update

--- handler/rootHandler.go
+++ handler/rootHandler.go
@@ -6,5 +6,16 @@ import (
 )
 
 func Homepage(w http.ResponseWriter, r *http.Request) {
-	templates.TMPL.ExecuteTemplate(w, "home.html", nil)
+	arr2 := [10]string{"Course",
+		"Chapter",
+		"Topic",
+		"Lecture",
+		"Resource",
+		"Section",
+		"Faculty",
+		"Department",
+		"Quiz",
+		"Assignment"}
+
+	templates.TMPL.ExecuteTemplate(w, "home.html", arr2)
 }
